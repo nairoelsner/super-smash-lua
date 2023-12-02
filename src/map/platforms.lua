fab25c4089c4754becd706aa31e2b91e83497316
Nairo Elsner <nairo.elsner@hotmail.com>
2023-12-02 14:47:01 -0300

    fix platform position

--- src/map/platforms.lua
+++ src/map/platforms.lua
@@ -4,5 +4,5 @@ platform1:setType('static')
 platform2 = world:newRectangleCollider(575, 200, 150, 50, {collision_class = "Platform"})
 platform2:setType('static')
 
-platform3 = world:newRectangleCollider(25, 200, 150, 50, {collision_class = "Platform"})
+platform3 = world:newRectangleCollider(75, 200, 150, 50, {collision_class = "Platform"})
 platform3:setType('static')
